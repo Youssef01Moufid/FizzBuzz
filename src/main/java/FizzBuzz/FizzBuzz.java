d23dfc76f8656c2ce0c9a986a145bc7617639737
Youssef01Moufid <ymoufid525@gmail.com>
2024-12-19 17:22:56 +0100

    Initial commit : ajout des fichiers du projet FizzBuzz

---
+++ src/main/java/FizzBuzz/FizzBuzz.java
@@ -0,0 +1,7 @@
+package FizzBuzz;
+
+public class FizzBuzz {
+    public String FizzBuzzde(int i) {
+        return "1" ;
+    }
+}
---
+++ src/test/java/FizzBuzzTest/FizzBuzzTest.java
@@ -0,0 +1,19 @@
+package FizzBuzzTest;
+
+import FizzBuzz.FizzBuzz;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.*;
+
+public class FizzBuzzTest {
+    @Test
+    public void testFizzBuzz1() {
+        //GIVEN
+        FizzBuzz fizzbuzz = new FizzBuzz();
+        //WHEN
+        String result = fizzbuzz.FizzBuzzde(1) ;
+        //THEN
+        assertThat(result).isEqualTo("1");
+    }
+}
